8496dbc35a54dc1c876e500b924458761c9351c5
Moaz Dawalibi <moaz.dawalibi@sdnone.net>
2024-02-24 14:32:28 +0300

    ز

--- src/Layout/Ui/Header.tsx
+++ src/Layout/Ui/Header.tsx
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {  MenuOutlined } from '@ant-design/icons';
 import WithDrawer from '../../HOC/WithDrawer';
-import { Button } from 'antd';
+import { Button, FloatButton } from 'antd';
 import { useTranslation } from 'react-i18next';
-import { FaArrowAltCircleRight } from 'react-icons/fa';
+import { FaArrowAltCircleRight, FaLanguage } from 'react-icons/fa';
 import { NavigationLinks } from './NavigationLinks';
 import useScrollEffects from './useScrollEffects';
+import { useChangeLanguage } from '../../Hooks/useChangeLanguage';
 
 const Header = () => {
   const [t] = useTranslation();
   useScrollEffects()
+  const { currentLanguage, changeLanguage } = useChangeLanguage();
+
     return (
       <header className='Header'>
         <Link to='/#Home'>
@@ -26,8 +29,14 @@ const Header = () => {
               button={<Button icon={<MenuOutlined />} type='primary' />}
             >
              <NavigationLinks className="DrawerLinks" />
+             
             </WithDrawer>
           </div>
+          <div className='floatButt'>
+            <FloatButton onClick={()=>{
+              currentLanguage === "en" ? changeLanguage("ar")  : changeLanguage("en")
+            }} icon={<FaLanguage />}  />
+            </div>
         </nav>
       </header>
     );
