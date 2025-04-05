73702b5246911db7ba5e01e5e1be2bdcc9ca8c0f
MoazDawalibi <moaazaldwalibi@gmail.com>
2025-04-05 21:40:39 +0300

    add number

--- src/Layout/Ui/Footer.tsx
+++ src/Layout/Ui/Footer.tsx
@@ -17,6 +17,7 @@ const Footer: React.FC = () => {
           <h1>{t('Contact Us')}</h1>
           {/* <h2>{t('Phone')}: 0958261912</h2> */}
           <h3>{t('Our Instagram')}: <Link to={"https://www.instagram.com/4leaf_shein?igsh=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr"}>@4Leaf</Link></h3>
+          <h3>{t('Our Number')}: <Link to={"https://www.instagram.com/4leaf_shein?igsh=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr"}>0984600159</Link></h3>
         </div>
       </div>
       <Divider />
